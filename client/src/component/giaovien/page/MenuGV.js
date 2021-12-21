6ca19b0a7534b4dc68ff2615b24af62105b2275a
unknown <tranphong804801@gmail.com>
2021-12-21 22:25:57 +0700

    vinh

--- client/src/component/giaovien/page/MenuGV.js
+++ client/src/component/giaovien/page/MenuGV.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import { NavLink } from "react-router-dom";
+import { DEFAULT_API } from "../../../conf/env";
+
 export default function MenuGV(props) {
+  const [listQA, setListQA] = useState([]);
+  const users_id = localStorage.getItem("userid");
+
   const change = (value) => {
     props.changleTitle(value);
   };
+  
+  const loadQA = () => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(`${DEFAULT_API}` +`qa/getbystatus?status=0&users_id=${users_id}`, requestOptions)
+      .then((response) => response.json())
+      .then((result) => setListQA(result))
+      .catch((error) => console.log("error", error));
+  };
+  console.log(listQA);
+  useEffect(() => {
+    loadQA();
+  }, []);
+
   return (
     <div>
       <div>
@@ -23,7 +46,7 @@ export default function MenuGV(props) {
                 <NavLink to="/giangvien/Answered">Answered</NavLink>
               </li>
               <li onClick={() => change("Not Answered")} className="menuvalue">
-                <NavLink to="/giangvien/NoteAnswered">Not Answered</NavLink>
+                <NavLink to="/giangvien/NoteAnswered">Not Answered</NavLink> {listQA.length != 0 ? <span class="header__cart-notice">?</span>:""}
               </li>
             </ul>
           </div>
